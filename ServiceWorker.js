0991d08fa94e410a602f6af4574484d6d435435a
Indie <indieworldbb@gmail.com>
2026-05-31 19:22:46 +0500

    Production_1.3.6

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-Bull3D_v0.3-0.2";
 const contentToCache = [
-    "Build/webgl_1.3.5.loader.js",
-    "Build/webgl_1.3.5.framework.js.unityweb",
-    "Build/webgl_1.3.5.data.unityweb",
-    "Build/webgl_1.3.5.wasm.unityweb",
+    "Build/webgl_1.3.6.loader.js",
+    "Build/webgl_1.3.6.framework.js.unityweb",
+    "Build/webgl_1.3.6.data.unityweb",
+    "Build/webgl_1.3.6.wasm.unityweb",
     "TemplateData/style.css"
 ];
 
